814eb4e09e06acc346512caf9cc4e16aa9ddefcb
Janani-V-S <82835111+Janani-V-S@users.noreply.github.com>
2021-05-02 13:19:50 +0530

    Week 3 Day 1 Assignments

---
+++ week3Day1Assignments/org/bank/BankInfo.java
@@ -0,0 +1,13 @@
+package org.bank;
+
+public class BankInfo {
+	public void fixed() {
+		System.out.println("Fixed");
+	}
+	public void savings() {
+		System.out.println("Savings");
+	}
+	public void deposit() {
+		System.out.println("Deposit - BankInfo Class");
+	}
+}
---
+++ week3Day1Assignments/org/student/Students.java
@@ -0,0 +1,20 @@
+package org.student;
+
+public class Students {
+
+	public void getStudentInfo(int ID) {
+		System.out.println("Method 1-Only ID");
+	}
+	public void getStudentInfo(int ID,String name) {
+		System.out.println("Method 1- ID & Name");
+	}
+	public void getStudentInfo(String email,long phoneNUm) {
+		System.out.println("Method 1-Email & Phone Number");
+	}
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
